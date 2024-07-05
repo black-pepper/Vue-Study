16c4701b11af687f077e59479d681a20f23fae92
Uju <56465463+black-pepper@users.noreply.github.com>
2024-07-05 13:44:08 +0900

    update projects

---
+++ projects/구구단/Gugudan.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: '',
+      result: '',
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      if (this.first*this.second == parseInt(this.value)) {
+        this.result = "정답";
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+      } else {
+        this.result = "오답";
+      }
+      this.value = '';
+      this.$refs.answer.focus();
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ first }}곱하기 {{ second }}는?</div>
+  <form v-on:submit="onSubmitForm">
+    <input v-model="value" ref="answer"/>
+    <button type="submit">입력</button>
+  </form>
+  <div id="result">{{result}}</div>
+</template>
---
+++ projects/끝말잇기/WordRelay.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      word: "시작",
+      value: '',
+      result: '',
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      if (this.word[this.word.length - 1] === this.value[0]) {
+        this.result = '정답';
+        this.word = this.value;
+      } else {
+        this.result = '오답';
+      }
+      this.value = '';
+      this.$refs.answer.focus();
+    }
+  }
+}
+</script>
+
+<template>
+  <div>현재 단어: {{ word }}</div>
+  <form v-on:submit="onSubmitForm">
+    <input v-model="value" ref="answer"/>
+    <button type="submit">입력</button>
+  </form>
+  <div id="result">{{result}}</div>
+</template>
