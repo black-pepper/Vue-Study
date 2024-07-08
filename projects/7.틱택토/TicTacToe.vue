82d02e05057c2b194396552b1464d5420250c4ea
Uju <56465463+black-pepper@users.noreply.github.com>
2024-07-08 16:51:37 +0900

    Add TicTacToe

---
+++ projects/7.틱택토/TicTacToe.vue
@@ -0,0 +1,42 @@
+<script>
+import TableComponent from './TableComponent.vue';
+export default {
+  components: {
+    TableComponent,
+  },
+  data() {
+    return {
+      tableData: [
+        ['', '', ''], 
+        ['', '', ''], 
+        ['', '', ''],
+      ],
+      turn: 'O',
+      winner: '',
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div>{{ turn }}님의 턴입니다.</div>
+    <table-component :table-data="tableData" />
+    <div v-if="winner">{{ winner }}님의 승리!</div>
+  </div>
+</template>
+
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
---
+++ projects/7.틱택토/TrComponent.vue
@@ -0,0 +1,25 @@
+<script>
+import TdComponent from './TdComponent.vue'
+
+export default {
+  components: {
+    TdComponent,
+  },
+  data() {
+    return {
+      parent: "-",
+    };
+  },
+  props: {
+    rowData: Array,
+    rowIndex: Number,
+  },
+}
+
+</script>
+
+<template>
+  <tr>
+    <td-component v-for="(cellData, index) in rowData" :key="index" :cell-data="cellData" :cell-index="index" :row-index="rowIndex"></td-component>
+  </tr>
+</template>
