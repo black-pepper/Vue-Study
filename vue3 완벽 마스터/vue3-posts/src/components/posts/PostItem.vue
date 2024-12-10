61392c2ba08ed96a0e4dc0300acef62eb72474a7
Uju <56465463+black-pepper@users.noreply.github.com>
2024-12-10 11:14:37 +0900

    페이지 컴포넌트에 Props 전달

--- vue3 완벽 마스터/vue3-posts/src/components/posts/PostItem.vue
+++ vue3 완벽 마스터/vue3-posts/src/components/posts/PostItem.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">{{ title }}</h5>
-      <p class="card-text">
-        {{ content }}
-      </p>
-      <a class="text-muted">{{ createdAt }}</a>
-    </div>
-  </div>
+  <AppCard>
+    <h5 class="card-title">{{ title }}</h5>
+    <p class="card-text">
+      {{ content }}
+    </p>
+    <a class="text-muted">{{ createdAt }}</a>
+  </AppCard>
 </template>
 
 <script setup>
+import AppCard from '../AppCard.vue'
 defineProps({
   title: {
     type: String,
--- vue3 완벽 마스터/vue3-posts/src/views/posts/PostDetailView.vue
+++ vue3 완벽 마스터/vue3-posts/src/views/posts/PostDetailView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h2>제목</h2>
-    <p>내용</p>
-    <p class="text-muted">{{ createdAt }}</p>
+    <h2>{{ form.title }}</h2>
+    <p>{{ form.content }}</p>
+    <p class="text-muted">{{ form.createdAt }}</p>
     <hr class="my-4" />
     <div class="row g-2">
       <div class="col-auto">
@@ -26,28 +26,24 @@
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
+import { getPostById } from '@/api/posts'
+import { ref } from 'vue'
 
-defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: [String, Date, Number],
-  },
+const props = defineProps({
+  id: Number,
 })
 
 const router = useRouter()
-const route = useRoute()
-const id = route.params.id
+const form = ref({})
 
+const fetchPost = () => {
+  const data = getPostById(props.id)
+  form.value = { ...data }
+}
+fetchPost()
 const goListPage = () => router.push({ name: 'PostList' })
-const goEditPage = () => router.push({ name: 'PostEdit', params: { id } })
+const goEditPage = () => router.push({ name: 'PostEdit', params: { id: props.id } })
 </script>
 
 <style lang="scss" scoped></style>
