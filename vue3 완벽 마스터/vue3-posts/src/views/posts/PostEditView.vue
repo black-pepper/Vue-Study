fc31554d9d68519aab67d27145ed86522fbe2140
Uju <56465463+black-pepper@users.noreply.github.com>
2024-12-27 14:09:46 +0900

    Alert 공통 컴포넌트

--- vue3 완벽 마스터/vue3-posts/src/views/posts/PostEditView.vue
+++ vue3 완벽 마스터/vue3-posts/src/views/posts/PostEditView.vue
@@ -8,6 +8,7 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
+    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
   </div>
 </template>
 
@@ -16,6 +17,7 @@ import { getPostById, updatePost } from '@/api/posts'
 import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import PostForm from '../../components/posts/PostForm.vue'
+import AppAlert from '@/components/AppAlert.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -41,13 +43,27 @@ fetchPost()
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value })
-    router.push({ name: 'PostDetail', params: { id } })
+    // router.push({ name: 'PostDetail', params: { id } })
+    vAlert('수정이 완료되었습니다.', 'success')
   } catch (error) {
     console.error(error)
+    vAlert('네트워크 오류!')
   }
 }
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } })
+
+const showAlert = ref(false)
+const alertMessage = ref('')
+const alertType = ref('error')
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true
+  alertMessage.value = message
+  alertType.value = type
+  setTimeout(() => {
+    showAlert.value = false
+  }, 2000)
+}
 </script>
 
 <style lang="scss" scoped></style>
