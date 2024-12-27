e86d781ecef95fb52dee94d6d383eed94d6d0203
Uju <56465463+black-pepper@users.noreply.github.com>
2024-12-27 17:51:05 +0900

    Vue3 플러그인 만들기

--- vue3 완벽 마스터/vue3-posts/src/main.js
+++ vue3 완벽 마스터/vue3-posts/src/main.js
@@ -3,6 +3,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
-
-createApp(App).use(router).mount('#app')
+import funcPlugins from './plugins/func'
+import objPlugins from './plugins/obj'
 import 'bootstrap/dist/js/bootstrap.js'
+import person from './plugins/person'
+
+const app = createApp(App)
+app.use(funcPlugins)
+app.use(objPlugins, { name: '짐코딩' })
+app.use(router)
+app.use(person, { name: '홍길동' })
+app.mount('#app')
--- vue3 완벽 마스터/vue3-posts/src/views/HomeView.vue
+++ vue3 완벽 마스터/vue3-posts/src/views/HomeView.vue
@@ -4,15 +4,31 @@
     <p>{{ $route }}</p>
     <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
   </div>
+  <hr class="my-4" />
+  <h2>{{ $person.name }}</h2>
+  <button class="btn btn-primary" @click="person.say">click person</button>
 </template>
 
+<script>
+export default {
+  created() {
+    // console.log(this.$person.name)
+    // this.$person.say()
+  },
+}
+</script>
+
 <script setup>
+import { inject } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const goAboutPage = () => {
   router.push('/about')
 }
+
+const person = inject('person')
+console.log('person.name: ', person.name)
 </script>
 
 <style lang="scss" scoped></style>
